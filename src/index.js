701ab71cab5bfde40f715ce11872eb6490f7d441
Viktoriia Zelenska <vikazgit@gmail.com>
2023-08-18 18:04:58 -0700

    feat: implement sorting method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
+module.exports = function towelSort(matrix) {
+  if (matrix) {
+    return matrix.reduce((acc, current, index) => {
+      return acc.concat((!(index % 2) ? current : current.reverse()));
+    
+    }, [])
+  }
   return [];
-}
+ }
